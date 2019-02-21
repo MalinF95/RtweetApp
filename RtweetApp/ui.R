371beb3b320a168d82baed93849aa6e6f724b092
malinfiedler <malin.fiedler@fs-students.de>
2019-02-21 11:46:29 +0100

    added rtweet functionalities
    Server doesn't work

--- RtweetApp/ui.R
+++ RtweetApp/ui.R
@@ -14,21 +14,14 @@ library(igraph)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Twitter Hashtag search"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+  # Some text input
+  textInput("hashtag", "Enter Hashtag here", ""),
     
-    # Show a plot of the generated distribution
+    # Show a plot of the network
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("NetworkPlot")
     )
   )
-))
+)
